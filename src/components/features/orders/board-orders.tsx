fc6cf87e57705bbffb394a5226d2ee711006c640
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2025-08-03 00:23:21 -0300

    feat(main): update order details with new product and format table number display

--- src/components/features/orders/board-orders.tsx
+++ src/components/features/orders/board-orders.tsx
@@ -5,18 +5,29 @@ export function BoardOrders() {
   const orders: Order[] = [
     {
       _id: '1',
-      table: '1',
+      table: '2',
       status: 'WAITING',
       products: [
         {
           _id: '1',
-          quantity: 2,
+          quantity: 1,
           product: {
             _id: '1',
             name: 'Bife',
             imagePath:
               'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            price: 36.95,
+            price: 40,
+          },
+        },
+        {
+          _id: '2',
+          quantity: 1,
+          product: {
+            _id: '2',
+            name: 'Coca-Cola Zero',
+            imagePath:
+              'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            price: 5.6,
           },
         },
       ],
--- src/components/features/orders/order-modal/order-modal-header.tsx
+++ src/components/features/orders/order-modal/order-modal-header.tsx
@@ -9,7 +9,7 @@ export function OrderModalHeader({ orderTable, onClose }: Props) {
   return (
     <header className="flex items-center justify-between">
       <h3 className="font-semibold text-h4 text-text leading-h2">
-        Mesa {orderTable}
+        Mesa {orderTable.padStart(2, '0')}
       </h3>
       <button
         className="flex cursor-pointer items-center justify-center rounded-full p-1 transition-all hover:bg-gray-100"
